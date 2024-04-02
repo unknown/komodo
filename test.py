2aef1403920bb89c6f4da229f94c863b8f70056a
David Mo <44306450+unknown@users.noreply.github.com>
2024-04-02 18:23:07 -0400

    Add optimizations flag to testing script

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import os
 import sys
 import subprocess
 
+# config
+verbose = True
+optimizations = False
+
 
 def compile_js(input_file_path, output_file_path):
     compile_res = subprocess.run(
@@ -18,16 +22,21 @@ def compile_js(input_file_path, output_file_path):
 
 
 def compile_c(input_file_path, output_file_path):
-    run_res = subprocess.run(
-        ["gcc", input_file_path, "-o", output_file_path], capture_output=True
-    )
+    args = ["gcc"]
+
+    if optimizations:
+        args.append("-O3")
+
+    args.extend([input_file_path, "-o", output_file_path])
+
+    run_res = subprocess.run(args, capture_output=True)
 
     if run_res.returncode != 0:
         raise Exception(f"GCC failed")
 
 
-def run_compiled(file_path):
-    run_res = subprocess.run([file_path], capture_output=True)
+def run_compiled(output_file_path):
+    run_res = subprocess.run([output_file_path], capture_output=True)
 
     if run_res.returncode != 0:
         raise Exception("Program failed")
@@ -36,8 +45,8 @@ def run_compiled(file_path):
     return output
 
 
-def run_node(file_path):
-    run_res = subprocess.run(["node", file_path], capture_output=True)
+def run_node(js_file_path):
+    run_res = subprocess.run(["node", js_file_path], capture_output=True)
 
     if run_res.returncode != 0:
         raise Exception("Node failed")
@@ -46,40 +55,39 @@ def run_node(file_path):
     return output
 
 
-def test_js(file_path):
+def test_js(js_file_path):
+    print(f"Testing {js_file_path}")
+
     c_file_path = "test/test.c"
     output_file_path = "test/test.out"
 
-    print(f"Testing {file_path}")
-    compile_js(file_path, c_file_path)
+    compile_js(js_file_path, c_file_path)
     compile_c(c_file_path, output_file_path)
-
     output = run_compiled(output_file_path)
-    expected_output = run_node(file_path)
+
+    expected_output = run_node(js_file_path)
 
     return (output, expected_output)
 
 
 def main():
-    verbose = True
-
     if len(sys.argv) != 2:
         print("Usage: python test.py <test-file>")
         exit(1)
 
-    test_file = sys.argv[1]
-    if not os.path.exists(test_file):
-        print(f"Could not find test file: {test_file}")
+    js_test_file = sys.argv[1]
+    if not os.path.exists(js_test_file):
+        print(f"Could not find test file: {js_test_file}")
         exit(1)
 
-    (output, expected_output) = test_js(test_file)
+    (output, expected_output) = test_js(js_test_file)
 
     if output == expected_output:
-        print(f"[✓] Test passed: {test_file}")
+        print(f"[✓] Test passed: {js_test_file}")
         if verbose:
             print(f"Output:\n{output}")
     else:
-        print(f"[✗] Test failed: {test_file}")
+        print(f"[✗] Test failed: {js_test_file}")
         print(f"Output:\n{output}")
         print(f"Expected output:\n{expected_output}")
 
